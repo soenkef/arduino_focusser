52115b100d2b3be715c4c003cf9155ea19527927
soenkef <mantu@gmx.de>
2021-04-23 20:26:42 +0200

    add dht for temp and humedity, prepare poti for stepper speed

--- arduino_focuser.ino
+++ arduino_focuser.ino
@@ -14,15 +14,48 @@
  todo:
  - make motor turn clockwise and other direction
  - set two buttons (forward, backward)
+ - set one button for display on
  - adapt movement of stepper by poti very sensible
+ - temperature
 
  */
+// Display
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+#include "DHT.h" //DHT Bibliothek laden
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+#define DHTPIN 7 //Der Sensor wird an PIN 2 angeschlossen    
+#define DHTTYPE DHT11    // Es handelt sich um den DHT11 Sensor
+DHT dht(DHTPIN, DHTTYPE); //Der Sensor wird ab jetzt mit „dth“ angesprochen
+
+// stepper
 #include <Stepper.h>
+
+// for potentiometer
+#define   POTI_PIN          A0
+int sensorReading = 0;
+int motorSpeed = 0;
+
+// temperature
+int tempSensorValue = 0;
+int temperature = 0;
+int tempIntervall = 500;
+
+
 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
 // for your motor
-
 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
@@ -31,13 +64,48 @@ int stepCount = 0;  // number of steps the motor has taken
 
 void setup() {
   // nothing to do inside the setup
+  dht.begin(); //DHT11 Sensor starten
+  Serial.begin(9600);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
 }
 
 void loop() {
-  // read the sensor value:
-  int sensorReading = analogRead(A0);
+
+  float humedity = dht.readHumidity(); //die Luftfeuchtigkeit auslesen und unter „Luftfeutchtigkeit“ speichern
+  float temperature = dht.readTemperature();//die Temperatur auslesen und unter „Temperatur“ speichern
+  delay(tempIntervall);
+  Serial.print("Temp: ");
+  //Serial.println(tempSensorValue);
+  Serial.println(temperature);
+  //Serial.print(" - ");
+  //temperature = map(tempSensorValue, 0, 410, -50, 150);
+  //Serial.print(" mapped temperature: ");
+  //Serial.println(temperature);
+  
+  // read the sensor value of poti:
+  sensorReading = analogRead(POTI_PIN);
+  Serial.print("Poti: ");
+  Serial.print(sensorReading);
+  Serial.print(" - ");
+ 
   // map it to a range from 0 to 100:
-  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+  motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+  //Serial.print("stepper: ");
+  Serial.println(motorSpeed);
+  float voltage = sensorReading * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.print("Volt: ");
+  Serial.println(voltage);
+
+  
+  Serial.println("------------------------------------");
+  delay(tempIntervall);
+  
   // set the motor speed:
   if (motorSpeed > 0) {
     myStepper.setSpeed(motorSpeed);
